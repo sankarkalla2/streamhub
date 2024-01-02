8789ef41e4c505beb67d4559de87d83cd07220d7
sankrkall2 <gowrisankarkalla4@gmail.com>
2024-01-02 22:21:27 +0530

    new commit

--- lib/block-service.ts
+++ lib/block-service.ts
@@ -45,15 +45,77 @@ export const blockUser = async (id: string) => {
       throw new Error("User not found");
     }
 
+    const isBlocked = await db.block.findUnique({
+      where: {
+        blockedId_blockerId: {
+          blockedId: otherUser.id,
+          blockerId: self.id,
+        },
+      },
+    });
+    if (isBlocked) {
+      throw new Error("User already blocked");
+    }
     const blockedUser = await db.block.create({
       data: {
         blockedId: otherUser.id,
         blockerId: self.id,
+      },
+      include: {
+        blocked: true,
+        blocker: true,
       },
     });
 
     return blockedUser;
   } catch (err: any) {
     throw new Error("Internel server error");
+  }
+};
+
+export const unblockUser = async (id: string) => {
+  try {
+    const self = await getSelf();
+    if (self.id === id) {
+      throw new Error("You can't unblock yourself");
+    }
+
+    const otherUser = await db.user.findUnique({
+      where: {
+        id,
+      },
+    });
+    if (!otherUser) {
+      throw new Error("User not found");
+    }
+
+    const isBlocked = await db.block.findUnique({
+      where: {
+        blockedId_blockerId: {
+          blockedId: otherUser.id,
+          blockerId: self.id,
+        },
+      },
+    });
+    if (!isBlocked) {
+      throw new Error("User already unblocked");
+    }
+
+    const unblock = await db.block.delete({
+      where: {
+        blockedId_blockerId: {
+          blockedId: otherUser.id,
+          blockerId: self.id,
+        },
+      },
+      include: {
+        blocked: true,
+        blocker: true,
+      },
+    });
+
+    return unblock;
+  } catch (err) {
+    throw new Error("Internel Error");
   }
 };
